91d4616799ca4432c2ac5dd9b2f775b3d48e35d3
sharry1712 <sharontempestini@gmail.com>
2024-06-19 15:50:15 +0200

    metodi di console

--- esercitazioni/prima_esercitazione/Program.cs
+++ esercitazioni/prima_esercitazione/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Sharon!");
+using System.Linq.Expressions;
+
+//Console.WriteLine("Hello, Sharon!");
+//Console.Write("inserisci il tuo nome \n"); 
+//Console.WriteLine("Sharon"); 
+//Console.WriteLine("premi un tasto");
+//Console.ReadKey(); // attende pressione per andare avanti
+
+string nome = "Sharon"; //legge quello che scrivo e lo memorizza nella variabile nome
+//Console.WriteLine(nome); // stampa a vdeo il contenuto sella variabile nome
+
+//Console.WriteLine("Hello," + nome + "!"); //concatenare più stringhe insieme alla varibile (non bisogno di appici mentre la scritta ne ha bisogno)
+//Console.WriteLine($"Hello, {nome}!");
+
+string cognome = "Tempestini";
+Console.WriteLine($"Hello, {nome} {cognome}!"); 
